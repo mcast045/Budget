e85be5737d2f59c331bf50d1770d843e6109e679
mcast045 <mcastaneda42@gmail.com>
2019-11-04 18:23:43 -0800

    Updates

---
+++ precache-manifest.db79a18732d81c7465a44e8a6d8c5956.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d1e7fa05fb39b7dbbd363df564ba9f",
+    "url": "/Budget/index.html"
+  },
+  {
+    "revision": "01b5924f9f4ce71ddf71",
+    "url": "/Budget/static/css/main.0c049be5.chunk.css"
+  },
+  {
+    "revision": "413258f435490bc61b71",
+    "url": "/Budget/static/js/2.1b198ce9.chunk.js"
+  },
+  {
+    "revision": "01b5924f9f4ce71ddf71",
+    "url": "/Budget/static/js/main.d456787a.chunk.js"
+  },
+  {
+    "revision": "d0aec3da779b3aba31df",
+    "url": "/Budget/static/js/runtime~main.8342eac9.js"
+  }
+]);
